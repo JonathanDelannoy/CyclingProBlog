802727a1739da92dd6d9b157a63d7a681faf24a9
JonathanDelannoy <58843893+JonathanDelannoy@users.noreply.github.com>
2020-03-30 15:56:08 +0200

    Add files via upload

---
+++ cyclingBlog/partial/footer.php
@@ -0,0 +1,65 @@
+<footer class="bg-dark">
+    <div class="container-fluid p-0 m-0">
+        <div class="row reseaux w-100 p-0 m-0">
+            <div class="col-12 d-flex justify-content-around my-1">
+                <div class="col-3">
+                    <a class="d-flex justify-content-center" href=""><i class="fab fa-instagram fa-2x"></i></a>
+                </div>
+                <div class="col-3">
+                    <a class="d-flex justify-content-center" href=""><i class="fab fa-twitter fa-2x"></i></a>
+                </div>
+                <div class="col-3">
+                    <a class="d-flex justify-content-center" href=""><i class="fab fa-facebook fa-2x"></i></a>
+                </div>
+                <div class="col-3">
+                    <a class="d-flex justify-content-center" href=""><i class="fab fa-snapchat fa-2x"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-around">
+            <div class="colonne col-4">
+                <div class="w-100 text-center my-1">
+                    <a class="color" href="">Calendrier 2020</a>
+                </div>
+                <div class="w-100 text-center mb-1">
+                    <a class="color" href="">Classement 2020</a>
+                </div>
+                <div class="w-100 text-center mb-1">
+                    <a class="color" href="">Prochaines Courses</a>
+                </div>
+            </div>
+            <div class="col-4 colonne column">
+                <div class="w-100 text-center my-1">
+                    <a class="color" href="">Giro 2020</a>
+                </div>
+                <div class="w-100 text-center mb-1">
+                    <a class="color" href="">Tour de France 2020</a>
+                </div>
+                <div class="w-100 text-center mb-1">
+                    <a class="color" href="">Vuelta 2020</a>
+                </div>
+            </div>
+            <div class="col-4 colonne column">
+                <div class="w-100 text-center my-1">
+                    <a class="color" href="https://www.economie.gouv.fr/dgccrf/Publications/Vie-pratique/Fiches-pratiques/Conditions-generales-de-vente">CGV</a>
+                </div>
+                <div class="w-100 text-center mb-1">
+                    <a class="color" href="stats.php">Statistiques du site</a>
+                </div>
+                <div class="w-100 text-center mb-1">
+                    <a class="color" href="mailto:delannoy.jonathan94@laposte.net">Contact</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ cyclingBlog/partial/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/fontawesome.min.js" integrity="sha256-7zqZLiBDNbfN3W/5aEI1OX/5uvck9V0yhwKOA9Oe49M=" crossorigin="anonymous"></script>
+    <title>CyclingPro Blog</title>
+</head>
+
+<body>
+
+<nav class="container-fluid header d-none d-lg-block m-0 p-0">
+    <section class="container-fluid bg-dark">
+        <div class="row">
+            <div class="col-11 p-0 m-0"></div>
+            <div class="col-1">
+                <a class="color d-flex justify-content-center" href="inscription.php">Inscription</a>
+            </div>
+        </div>
+    </section>
+    <div class="row headcontain p-2">
+        <div class="col-lg-12 col-sm-6 col-md-6 col-6 h-100 p-0 m-0">
+            <ul class="d-flex justify-content-between align-items-center h-100 navig p-0 m-0 mx-3">
+                <li><a class="nav" href="index.php">Accueil</a></li>
+                <li><a class="nav" href="results.php">Les résultats</a></li>
+                <li><a class="nav" href="articles.php">Les Actualités</a></li>
+                <li><a class="nav" href="videos.php">Les Vidéos</a></li>
+                <li><a class="nav" href="shopping.php">La boutique</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="menu d-lg-none sticky"><span></span></div>
+
+<nav id="nav">
+    <ul class="col-6">
+        <li><a class="nav" href="index.php">Accueil</a></li>
+        <li><a class="nav" href="result.php">Les résultats</a></li>
+        <li><a class="nav" href="articles.php">Les Actualités</a></li>
+        <li><a class="nav" href="shopping.php">La boutique</a></li>
+        <li><a class="nav" href="inscription.php">Inscription</a></li>
+    </ul>
+</nav>
+<div class="overlay"></div>
+
+
+
+<div class="col-2 p-0 m-0">
+    <a href="chat-users.php"><button class="chatbox mr-2 btn-white">ChatBox Utilisateur</button></a>
+</div>
+
+<div class="col-2 p-0 m-0">
+    <a href="chat-coureurs.php"><button class="chatbox2 ml-2 btn-white">ChatBox Coureur</button></a>
+</div>
